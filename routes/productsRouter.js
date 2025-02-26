9bc894c7bf66b9335545cf7ddc714201e7883d69
RoggerMen <130023813+RoggerMen@users.noreply.github.com>
2025-02-26 18:24:46 -0500

    TERMINAMOS EL CRUD COMPLETO, AQUI VIMOS EL POST, PATCH Y PUT

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -62,6 +62,43 @@ const router = express.Router();
     })
   });
 
+  // el "PATCH" ES MUY PARECIDO A NUESTRO "POST" PORQUE TODO LO VA A RECIBIR EN UN CUERPO
+  // LA UNICA DIFERENCIA ES QUE AQUI RECIBIMOS UN "id" EL ID DEL PRODUCTO QUE YO QUIERO EDITAR
+  // EL "PATCH" NO NOS OBLIGA A ENVIAR TODOS LOS CAMPOS O ATRIBUTOS DEL PRODUCTO A ACTUALIZAR SINO QUE ES MAS FLEXIBLE Y PODEMOS ENVIARLE SOLO LOS QUE QUEREMOS ACTUALIZAR Y NO TODO EL OBJETO COMO LO HACE EL "PUT"
+  router.patch('/:id', (req, res) => {
+    const id = req.params.id;
+    // PEDIMOS TODO EL CUERPO
+    const body = req.body;
+    res.json({
+      message: 'update some products field',
+      data: body,
+      id
+    })
+  });
+
+
+  // EL "PUT" ES EXACTAMENTE IGUAL SOLO QUE NOS PIDE TODO EL OBJETO, TODO LOS CAMPOS Y ATRIBUTOS QUE SEAN COMPLETADOS PARA PODER ACTUALIZAR
+  router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    // PEDIMOS TODO EL CUERPO
+    const body = req.body;
+    // PODEMOS HACERLO DE ESTA MANERA O COMO EL DEL PATCH
+    res.json({
+      message: 'update all products field with id: ' + id,
+      data: {...body,
+      id}
+    })
+  });
+
+  // EL "DELETE" ES TAMBIEN SIMILAR AL PUT Y PATCH SOLO QUE NO VA A RECIBIR UN CUERPO SOLO EL "id"
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    res.json({
+      message: 'DELETED PRODUCT',
+      id
+    })
+  });
+
 
 // CADA UNO DE LAS RUTAS TENDRIA UN MODULO EN ESTE CASO "router"
 module.exports = router;
